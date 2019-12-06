83b3dbf929087cb3ad399f5adecdbb3aaec891ea
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-12-05 21:58:12 -0700

    Pokemon 1ra Version

---
+++ Pokedex/app/src/main/java/com/example/pokedex/pokeapi/PokemonService.java
@@ -0,0 +1,12 @@
+package com.example.pokedex.pokeapi;
+
+import com.example.pokedex.models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokemonService {
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit")int limit, @Query("offset")int offset);
+}
